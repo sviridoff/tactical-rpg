083cd019062e5e4696a6fd0ea193e027caa42cc0
sviridoff <sviridoff.mail@gmail.com>
2018-05-02 23:47:41 +0200

    added immer as dependency

--- src/reducers/actors.ts
+++ src/reducers/actors.ts
@@ -1,5 +1,4 @@
-import clone from "clone";
-
+import produce from "immer";
 import Actor from "../models/Actors";
 
 const teams: any = [
@@ -15,78 +14,66 @@ const teams: any = [
 const initialState: TActors = new Actor(teams).get();
 
 export function actors(state = initialState, action: any) {
-  switch (action.type) {
-    case "UPDATE_ACTOR_CURRENT_POSITION": {
-      const stateClone = clone(state);
-      const { id } = action.data.actor;
-      const { x, y } = action.data.tile;
-      const actor = stateClone[id];
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "UPDATE_ACTOR_CURRENT_POSITION": {
+        const { id } = action.data.actor;
+        const { x, y } = action.data.tile;
+        const actor = draft[id];
 
-      actor.currentPosition = { x, y };
+        actor.currentPosition = { x, y };
 
-      return stateClone;
-    }
-    case "UPDATE_ACTOR_ORIGINAL_POSITION": {
-      const stateClone = clone(state);
-      const { id } = action.data.actor;
-      const actor = stateClone[id];
-
-      actor.originalPosition = actor.currentPosition;
-
-      return stateClone;
-    }
-    case "ATTACK_ENEMY_ACTOR": {
-      const stateClone = clone(state);
-      const actor = stateClone[action.data.actor.id];
-      const enemyActor = stateClone[action.data.enemyActor.id];
-
-      enemyActor.healthPoints -= actor.damage;
-
-      if (enemyActor.healthPoints <= 0) {
-        enemyActor.isDead = true;
+        break;
       }
+      case "UPDATE_ACTOR_ORIGINAL_POSITION": {
+        const { id } = action.data.actor;
+        const actor = draft[id];
 
-      return stateClone;
+        actor.originalPosition = actor.currentPosition;
+
+        break;
+      }
+      case "ATTACK_ENEMY_ACTOR": {
+        const actor = draft[action.data.actor.id];
+        const enemyActor = draft[action.data.enemyActor.id];
+
+        enemyActor.healthPoints -= actor.damage;
+
+        if (enemyActor.healthPoints <= 0) {
+          enemyActor.isDead = true;
+        }
+
+        break;
+      }
+      case "UPDATE_ACTOR_ATTACK_TARGET": {
+        const actor = draft[action.data.actor.id];
+        const enemyActor = draft[action.data.enemyActor.id];
+
+        actor.isGoingToAttack = true;
+        enemyActor.isGoingToBeAttacked = true;
+
+        break;
+      }
+      case "FLUSH_ACTORS_ATTACK_TARGET": {
+        Object.values(draft).forEach((actor) => {
+          actor.isGoingToBeAttacked = false;
+          actor.isGoingToAttack = false;
+        });
+
+        break;
+      }
+      case "DISABLE_ACTOR": {
+        const actor = draft[action.data.actor.id];
+
+        actor.isDisable = true;
+      }
+      case "ENABLE_ALL_ACTORS": {
+        Object.values(draft).forEach((actor) => {
+          actor.isDisable = false;
+        });
+
+        break;
+      }
     }
-    case "UPDATE_ACTOR_ATTACK_TARGET": {
-      const stateClone = clone(state);
-      const actor = stateClone[action.data.actor.id];
-      const enemyActor = stateClone[action.data.enemyActor.id];
-
-      actor.isGoingToAttack = true;
-      enemyActor.isGoingToBeAttacked = true;
-
-      return stateClone;
-    }
-    case "FLUSH_ACTORS_ATTACK_TARGET": {
-      const stateClone = clone(state);
-
-      Object.keys(stateClone).forEach((key) => {
-        stateClone[key].isGoingToBeAttacked = false;
-        stateClone[key].isGoingToAttack = false;
-      });
-
-      return stateClone;
-    }
-    case "DISABLE_ACTOR": {
-      const stateClone = clone(state);
-      const actor = stateClone[action.data.actor.id];
-
-      actor.isDisable = true;
-
-      return stateClone;
-    }
-    case "ENABLE_ALL_ACTORS": {
-      const stateClone = clone(state);
-
-      Object.keys(stateClone).forEach((key) => {
-        stateClone[key].isDisable = false;
-      });
-
-      return stateClone;
-    }
-    default: {
-      return state;
-    }
-  }
+  });
 }
--- src/reducers/player.ts
+++ src/reducers/player.ts
@@ -1,3 +1,4 @@
+import produce from "immer";
 import { get } from "lodash-es";
 
 const initialState: TPlayer = {
@@ -10,43 +11,58 @@ const initialState: TPlayer = {
 };
 
 export function player(state = initialState, action: any) {
-  switch (action.type) {
-    case "UPDATE_PLAYER_ACTIVE_ACTOR_ID": {
-      const activeActorId = get(action, "data.actor.id");
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "UPDATE_PLAYER_ACTIVE_ACTOR_ID": {
+        draft.activeActorId = get(action, "data.actor.id");
 
-      return { ...state, activeActorId };
-    }
-    case "UPDATE_PLAYER_SELECTED_ACTOR_ID": {
-      const selectedActorId = get(action, "data.actor.id");
+        break;
+      }
+      case "UPDATE_PLAYER_SELECTED_ACTOR_ID": {
+        draft.selectedActorId = get(action, "data.actor.id");
 
-      return { ...state, selectedActorId };
+        break;
+      }
+      case "DISABLE_PLAYER_IS_PLAYER_TURN": {
+        draft.isPlayerTurn = false;
+
+        break;
+      }
+      case "ENABLE_PLAYER_IS_PLAYER_TURN": {
+        draft.isPlayerTurn = true;
+
+        break;
+      }
+      case "SHOW_PLAYER_TURN_BANNER": {
+        draft.showTurnBanner = true;
+
+        break;
+      }
+      case "HIDE_PLAYER_TURN_BANNER": {
+        draft.showTurnBanner = false;
+
+        break;
+      }
+      case "PLAYER_WIN": {
+        draft.isBattleWon = true;
+
+        break;
+      }
+      case "PLAYER_LOSE": {
+        draft.isBattleWon = false;
+
+        break;
+      }
+      case "SHOW_PLAYER_BATTLE_END_BANNER": {
+        draft.showBattleEndBanner = true;
+
+        break;
+      }
+      case "HIDE_PLAYER_BATTLE_END_BANNER": {
+        draft.showBattleEndBanner = false;
+
+        break;
+      }
     }
-    case "DISABLE_PLAYER_IS_PLAYER_TURN": {
-      return { ...state, isPlayerTurn: false };
-    }
-    case "ENABLE_PLAYER_IS_PLAYER_TURN": {
-      return { ...state, isPlayerTurn: true };
-    }
-    case "SHOW_PLAYER_TURN_BANNER": {
-      return { ...state, showTurnBanner: true };
-    }
-    case "HIDE_PLAYER_TURN_BANNER": {
-      return { ...state, showTurnBanner: false };
-    }
-    case "PLAYER_WIN": {
-      return { ...state, isBattleWon: true };
-    }
-    case "PLAYER_LOSE": {
-      return { ...state, isBattleWon: false };
-    }
-    case "SHOW_PLAYER_BATTLE_END_BANNER": {
-      return { ...state, showBattleEndBanner: true };
-    }
-    case "HIDE_PLAYER_BATTLE_END_BANNER": {
-      return { ...state, showBattleEndBanner: false };
-    }
-    default: {
-      return state;
-    }
-  }
+  });
 }
